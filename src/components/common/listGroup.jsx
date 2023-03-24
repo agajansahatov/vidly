213f67bfd3a59e4a41a62d4fb8a2bb4add632be6
Agajan Sahatov <agajan.st@gmail.com>
2023-03-24 18:52:08 +0800

    Build Filtering with defaultProps

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ListGroup = (props) => {
+	const { items, textProperty, valueProperty, selectedItem, onItemSelect } =
+		props;
+
+	return (
+		<ul className="list-group">
+			{items.map((item) => (
+				<li
+					key={item[valueProperty] ? item[valueProperty] : "allGenres"}
+					className={
+						item === selectedItem ? "list-group-item active" : "list-group-item"
+					}
+					style={{ cursor: "pointer" }}
+					onClick={() => onItemSelect(item)}
+				>
+					{item[textProperty]}
+				</li>
+			))}
+		</ul>
+	);
+};
+
+ListGroup.defaultProps = {
+	textProperty: "name",
+	valueProperty: "_id",
+};
+
+export default ListGroup;

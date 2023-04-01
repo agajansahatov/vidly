a9c67f69407c0f8775578cd245fa7bcdd321e88f
Agajan Sahatov <agajan.st@gmail.com>
2023-04-01 17:13:42 +0800

    Build the registerForm

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
 
-class LoginForm extends Form {
+class RegisterForm extends Form {
 	constructor() {
 		super();
-		this.state.data = { username: "", password: "" };
+		this.state.data = { username: "", password: "", name: "" };
 	}
 
 	schema = {
-		username: Joi.string().required().label("Username"),
-		password: Joi.string().required().label("Password"),
+		username: Joi.string().required().email().label("Username"),
+		password: Joi.string().required().min(5).label("Password"),
+		name: Joi.string().required().label("Name"),
 	};
 
 	doSubmit = (e) => {
@@ -21,16 +22,17 @@ class LoginForm extends Form {
 
 	render() {
 		return (
-			<div style={{ maxWidth: "600px", minWidth: "400px" }}>
-				<h1>Login</h1>
+			<div>
+				<h1>Register</h1>
 				<form onSubmit={this.doSubmit}>
 					{this.renderInput("username", "Username")}
 					{this.renderInput("password", "Password", "password")}
-					{this.renderButton("Login")}
+					{this.renderInput("name", "Name")}
+					{this.renderButton("Register")}
 				</form>
 			</div>
 		);
 	}
 }
 
-export default LoginForm;
+export default RegisterForm;
